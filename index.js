e5553f04ade4e1dad4acff54dfa5755915f99804
KevinMaciasR <wilter2607@gmail.com>
2020-12-23 21:28:00 -0500

    Diseño del Frontend

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const http =require('http')
-http.createServer(function(req,resp){ //req: datos que envia el cliente resp: datos que se envian hcia al cliente 
-resp.write("<h1>Hola</h1>")
-}).listen(8080);//puerto del servidor web
+const express=require('express')
+const server=express();//Crea el servidor
+server.get('/', function(req,resp){//req: datos que envia el cliente al servidor |||| resp: datos que se envian hcia al cliente 
+    resp.send("<h1>Hola</h1>")
+    
+})
+server.listen(8080,//puerto del servidor web es 8080
+()=>{console.log("Iniciar Página web con http://localhost:8080")})
