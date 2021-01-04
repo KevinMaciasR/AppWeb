b6189b5f346398c12aeec97d276da959037e71cf
KevinMaciasR <wilter2607@gmail.com>
2021-01-03 22:10:56 -0500

    Conectando el login a la base de datos

--- controllers/usuariosControl.js
+++ controllers/usuariosControl.js
@@ -2,7 +2,6 @@ const db = require('./database');
 const usuariosControl={};
 
 usuariosControl.getUsuarios = (req,res)=>{ //desde la Base de Datos. Get de todos los usuarios
-    console.log("Get de usuarios")
     db.query("Select * FROM usuario", (err,result,fields)=>{
         if (err) {
             res.status(500).send(err);
@@ -10,6 +9,32 @@ usuariosControl.getUsuarios = (req,res)=>{ //desde la Base de Datos. Get de todo
             return;
         }
         res.json(result);
+    });
+}
+/*usuariosControl.getUsuario = (req,res)=>{ //usando Base de Datos, consulta con "nombre"
+console.log(("Usuario consultado: "+req.params.nombre));
+    db.query('SELECT * FROM usuario WHERE nombre=?',req.params.nombre, 
+    (err,result,fields)=>{
+        if (err) {
+            res.status(500).send(err);
+            console.log(err);
+            return;
+        }
+        res.json(result);
+    });
+}*/
+usuariosControl.getUsuario = (req,res)=>{ //usando Base de Datos, consulta con "email"
+console.log(("Usuario consultado: "+req.params.email));
+    db.query('SELECT * FROM usuario WHERE email=?',req.params.email, 
+    (err,result,fields)=>{
+        if (err) {
+            res.status(500).send(err);
+            console.log(err);
+            return;
+        }
+        console.log(result+" Antes")
+        res.json(result);
+        
     });
 }
 usuariosControl.postUsuario = (req,res)=>{ //desde la Base de datos. Para Añadir Usuario
@@ -25,7 +50,7 @@ usuariosControl.postUsuario = (req,res)=>{ //desde la Base de datos. Para Añadi
     db.query("INSERT INTO usuario SET ?", [SQLbody],
     (err,result)=>{
         if (err) {
-            res.status(500).send(err);
+            res.status(500).send(err+"Error-Status");//error de usuario duplicado
             console.log(err);
             return;
         }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,15 @@
 const {Router}=require('express');
 const router = Router();
-const {getUsuarios,postUsuario,putUsuario,deleteUsuario} = require('../controllers/usuariosControl');
+const {getUsuarios,getUsuario,postUsuario,putUsuario,deleteUsuario} = require('../controllers/usuariosControl');
 
 router.route('/')
 //llama a la funciones que se encuentran en el archivo registrosControl
    .get(getUsuarios)
    .post(postUsuario);
-router.route('/:nombre')   
+router.route('/:nombre')// este '/:nombre' será usado en la sintaxis del query para selecionar el nombre de la columna de la tabla
+     //.get(getUsuario)   
      .put(putUsuario)
      .delete(deleteUsuario);
-
+router.route('/:email')
+     .get(getUsuario)
 module.exports=router
